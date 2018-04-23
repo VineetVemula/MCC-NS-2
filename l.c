ee5c38aa8b81a68127b402d46d38d0d1a8bf38c8
Vineet Vemula <vindare@gmail.com>
2018-04-23 22:24:24 +0530

    Create l.c

---
+++ l.c
@@ -0,0 +1,97 @@
+#include<stdio.h>
+
+#include<stdlib.h>
+
+#include<string.h>
+
+int isKeyword(char temp[])
+
+{
+
+	if(strcmp(temp,"int")==0||strcmp(temp,"void")==0||strcmp(temp,"float")==0||strcmp(temp,"main")==0||strcmp(temp,"return")==0)
+
+		return 1;
+
+	else 
+
+		return 0;
+
+}
+
+int isOperator(char temp[])
+
+{
+
+	if(strcmp(temp,"+")==0||strcmp(temp,"=")==0||strcmp(temp,"-")==0||strcmp(temp,"/")==0||strcmp(temp,"*")==0)
+
+		return 1;
+
+	else 
+
+		return 0;
+
+}
+
+int isDel(char temp[])
+
+{
+
+	if(strcmp(temp,";")==0||strcmp(temp,",")==0||strcmp(temp,"(")==0||strcmp(temp,")")==0||strcmp(temp,"{")==0||strcmp(temp,"}")==0)
+
+		return 1;
+
+	else 
+
+		return 0;
+
+}
+
+void lex(char temp[])
+
+{
+
+	int i,j;
+
+	if(isKeyword(temp))
+
+		printf("keyword: %s\n",temp);
+
+	else if(isOperator(temp))
+
+		printf("Operator: %s\n",temp);
+
+	else if(isDel(temp))
+
+		printf("Delimeter: %s\n",temp);
+
+	else
+
+		printf("Variable: %s\n",temp);
+
+}
+
+void main()
+
+{
+
+    char temp[20];
+
+    FILE *fp;
+
+    fp=fopen("program.txt" ,"r");
+
+    while(!feof(fp))
+
+    {
+
+        fscanf(fp,"%s",temp);
+
+        //printf("%s\n",temp);
+
+	lex(temp);
+
+    }
+
+    fclose(fp);
+
+}
